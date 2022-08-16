f8e601eb59f601ff15b1b2085fddbf11e661551e
Vinicius Gomes <viniplay0599@gmail.com>
2022-08-16 01:48:10 -0300

    feat: implements configuration to encrypt passwords

--- src/main/java/com/viniciusgomes/restapispringmongodb/controllers/UserController.java
+++ src/main/java/com/viniciusgomes/restapispringmongodb/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.viniciusgomes.restapispringmongodb.models.User;
 import com.viniciusgomes.restapispringmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +14,14 @@ import java.util.List;
 @RestController
 @RequestMapping(value="/users")
 public class UserController {
+    private final PasswordEncoder encoder;
     @Autowired
     private UserService service;
+
+    public UserController(PasswordEncoder encoder) {
+        this.encoder = encoder;
+    }
+
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
         List<User> users = service.findAll();
