5b7ffef278f481d74063e113a737260e5297cbb2
Vinicius Gomes <viniplay0599@gmail.com>
2022-08-15 23:42:16 -0300

    project created

---
+++ src/test/java/com/viniciusgomes/restapispringmongodb/RestApiSpringMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciusgomes.restapispringmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiSpringMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
