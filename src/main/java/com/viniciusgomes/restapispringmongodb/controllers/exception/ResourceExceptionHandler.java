777945d4678b3b78be030275eb6a56f8a4629a13
Vinicius Gomes <viniplay0599@gmail.com>
2022-08-16 15:24:56 -0300

    feat: implements exceptions handlers

---
+++ src/main/java/com/viniciusgomes/restapispringmongodb/controllers/exception/ResourceExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.viniciusgomes.restapispringmongodb.controllers.exception;
+
+import com.viniciusgomes.restapispringmongodb.services.exception.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException error, HttpServletRequest req) {
+        HttpStatus code = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(System.currentTimeMillis(), code.value(), "Not found", error.getMessage(), req.getRequestURI());
+        return ResponseEntity.status(code).body(err);
+    }
+}
